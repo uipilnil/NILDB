7a060344913dc79c13f72488e9aec65db7fdbdec
uipilnil <uipilnil@163.com>
2026-06-10 11:23:37 +0800

    字段信息管理器

--- src/main/java/io/github/uipilnil/backend/utils/Parser.java
+++ src/main/java/io/github/uipilnil/backend/utils/Parser.java
@@ -1,6 +1,9 @@
 package io.github.uipilnil.backend.utils;
 
+import com.google.common.primitives.Bytes;
+
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
  * 数据类型转换工具类
@@ -21,6 +24,12 @@ public class Parser {
         return buffer.getLong();
     }
 
+    public static ParseStringRes parseString(byte[] raw) {
+        int length = parseInt(Arrays.copyOf(raw, 4));
+        String str = new String(Arrays.copyOfRange(raw, 4, 4 + length));
+        return new ParseStringRes(str, length + 4);
+    }
+
     public static byte[] short2Byte(short value) {
         return ByteBuffer.allocate(Short.SIZE / Byte.SIZE).putShort(value).array();
     }
@@ -31,5 +40,19 @@ public class Parser {
 
     public static byte[] long2Byte(long value) {
         return ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(value).array();
+    }
+
+    public static byte[] string2Byte(String str) {
+        byte[] l = int2Byte(str.length());
+        return Bytes.concat(l, str.getBytes());
+    }
+
+    public static long string2Uid(String key) {
+        long seed = 13331;
+        long res = 0;
+        for (byte b : key.getBytes()) {
+            res = res * seed + (long) b;
+        }
+        return res;
     }
 }
