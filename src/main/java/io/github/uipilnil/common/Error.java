07b49c42eb586a2c126c98cb094b7572a7dfd01a
uipilnil <uipilnil@163.com>
2026-06-10 16:54:39 +0800

    表管理器

--- src/main/java/io/github/uipilnil/common/Error.java
+++ src/main/java/io/github/uipilnil/common/Error.java
@@ -29,6 +29,8 @@ public class Error {
     public static final Exception FieldNotFoundException = new RuntimeException("Field not found!");
     public static final Exception InvalidLogOpException = new RuntimeException("Invalid logic operation!");
     public static final Exception InvalidValuesException = new RuntimeException("Invalid values!");
+    public static final Exception DuplicatedTableException = new RuntimeException("Duplicated table!");
+    public static final Exception TableNotFoundException = new RuntimeException("Table not found!");
 
     // parser
     public static final Exception InvalidCommandException = new RuntimeException("Invalid command!");
